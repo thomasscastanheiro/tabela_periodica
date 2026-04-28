5a8a4f06a66a39b2386d6ed250099941381fd3f5
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2026-04-28 19:26:41 -0300

    quase feito

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 const tabela = document.getElementById("tabela")
 
 colecaoElementos.forEach((elemento) => {
-    tabela.rows[elemento.linha-1].cells[elemento.coluna-1].innerHTML = `
+    const celula = tabela.rows[elemento.linha-1].cells[elemento.coluna-1]
+    celula.innerHTML = `
     <h4>${elemento.numeroAtomico}</h4>
     <h3>${elemento.simbolo}</h3>
     <p>${elemento.nome}</p>
     <p>${elemento.massaAtomica}</p>`
-    tabela.rows[elemento.linha-1].cells[elemento.coluna-1].style.backgroundColor = "#"+elemento.corGrupo
+    tabela.rows[elemento.linha-1].cells[elemento.coluna-1].style.backgroundColor = "#"+elemento.corHexCpk
+    celula._elemento = elemento
+    
+    
+})          
 
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+document.querySelectorAll('td').forEach(td => {
+        td.addEventListener("click", (event) => {
+        const celula = event.currentTarget;
+        const elemento = celula._elemento;
+        if (!elemento) return;
+        detalhes.style.display = "block"
+        
+        document.getElementById("detalhes").innerHTML =`
+            <button id="fechar">X</button>
+            <h1>${elemento.nome}</h1>`
+
+        document.getElementById("fechar").addEventListener("click", () => {
+        document.getElementById("detalhes").style.display = "none"
+})
+})
+        
+})
+
